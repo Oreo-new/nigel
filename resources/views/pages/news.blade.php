3b6cd65f17d16cff7f7122c7daf1693fe4ce5d69
Clint Responso <clint.responso@copypaste.ph>
2023-11-22 12:48:25 +0800

    fixing news page

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -15,7 +15,7 @@
                         @foreach($allnews  as $item)
                             <div class="article-item w-full mb-3 mt-10">
                                 <div class="head border-b border-gray-200">
-                                    <h4 class="text-xl font-bold hover:text-red-600 transition-colors "> <a href="tbm-articles/{{$item->slug}} ">{{$item->title}}</a></h4>
+                                    <h4 class="text-xl font-bold hover:text-red-600 transition-colors "> <a href="news/{{$item->slug}} ">{{$item->title}}</a></h4>
                                     <div class="flex items-center my-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-red-600 mr-2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
