7f98d8e4579781b00affb12abe0fc8b73d01d009
Clint Responso <clint.responso@copypaste.ph>
2025-01-14 12:49:50 +0800

    adding googl tag manager code.

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -14,6 +14,14 @@
         <!-- Styles -->
         
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PLDCD92P');</script>
+        <!-- End Google Tag Manager -->
         @livewireStyles
         <link rel="shortcut icon" href="{{ asset('/images/favicon.ico') }}">
         <!-- Scripts -->
@@ -56,6 +64,10 @@
     </head>
   
     <body class="font-serif antialiased {{ bodyClass() }}" >
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLDCD92P"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         
         <div class="min-h-screen bg-white">
             {{-- <x-header /> --}}
